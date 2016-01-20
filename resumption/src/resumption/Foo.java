a789ad1a33b571bde08cd6c9524485645d84a349
Cabel <cabel@u-10-81-137-231.dhcp.unlv.edu>
2016-01-20 15:24:23 -0800

    Foo: corrected errors

--- resumption/src/resumption/Foo.java
+++ resumption/src/resumption/Foo.java
@@ -5,7 +5,6 @@ public class Foo extends AbstractCSProcess {
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-
 	}
 
 	int $b1$a; // original a
@@ -15,35 +14,61 @@ public class Foo extends AbstractCSProcess {
 
 	@Override
 	protected void start() {
-	// interface 0 (int x, int y) int $i0$x, $i0$y;
-	// interface 1 (int z)
-	int $i1$z;
-	switch (control(0)) { 
-	case 0:
-		// interface 0 (int x, int y) 
-		resume(Integer.class, Integer.class); 
-		$i0$x = getParameter(0);
-		$i0$y = getParameter(1);
-		System.out.println("executing Block 1 code"); //B1
-	case 1:
-		if (isRunning()) {
-//			$c1 = B2; 
+
+		int $i0$x, $i0$y; // interface 0 (int x, int y)
+		int $i1$z; // interface 1 (int z)
+
+		switch (control(0)) {
+		case 0:
+			resume(Integer.class, Integer.class); // interface 0 (int x, int y)
+
+			$i0$x = getParameter(0);
+			$i0$y = getParameter(1);
+
+			B1(); //B1
+
+		case 1:
+			if (isRunning()) {
+				 $c1 = B2();
 			}
-		$1: while ($c1) { 
-			switch (control(1)) { 
+			$1: while ($c1) {
+				switch (control(1)) {
 				case 0:
-					System.out.println("executing Block 3 code"); //B3
-					suspend(1,1); return;
+					B3(); // B3
+					suspend(1, 1);
+					return;
 				case 1:
-					//interface 1 (int z) 
-					resume(Integer.class); 
-					$i1$z = getParameter(0); 
-//					B4
-					System.out.println("executing Block 4 code"); //B4
-//					$c1 = B2;
-				} // end switch 
+					resume(Integer.class); // interface 1 (int z)
+					$i1$z = getParameter(0);
+					B4(); // B4
+					 $c1 = B2();
+				} // end switch
 			} // end while
-			System.out.println("executing Block 5 code"); //B5
+
+			B5(); // B5
+
 		} // end switch
 	} // end method start
+	
+	private void B1() {
+		System.out.println("executing Block 1 code");
+	}
+
+	private boolean B2() {
+		System.out.println("executing Block 2 code"); 
+		return true;
+	}
+
+	private void B3() {
+		System.out.println("executing Block 3 code");
+	}
+
+	private void B4() {
+		System.out.println("executing Block 4 code");
+	}
+
+	private void B5() {
+		System.out.println("executing Block 5 code");
+	}
+
 } // end class foo
